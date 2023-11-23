b9fc8cb72693919d4acba89596693c97065438b7
ShadePrima <aleksandrag1984@gmail.com>
2023-11-23 12:08:59 +0200

    add drawer

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -7,6 +7,90 @@ class MyDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       backgroundColor: Theme.of(context).colorScheme.background,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              DrawerHeader(
+                child: Icon(
+                  Icons.favorite,
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              //home tile
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.home,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                  title: const Text("H O M E"),
+                  onTap: () {
+                    //this is already the home screen so just pop drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+
+              //prifile tile
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.person,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                  title: const Text("P R O F I L E"),
+                  onTap: () {
+                    //this is already the home screen so just pop drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+
+              //user tile
+              Padding(
+                padding: const EdgeInsets.only(left: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.group,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                  title: const Text("U S E R S"),
+                  onTap: () {
+                    //this is already the home screen so just pop drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+            ],
+          ),
+          //drawer header
+
+          //logoun tile
+          Padding(
+            padding: const EdgeInsets.only(left: 25),
+            child: ListTile(
+              leading: Icon(
+                Icons.logout,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+              title: const Text("L O G O U T"),
+              onTap: () {
+                //this is already the home screen so just pop drawer
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
